f286d73b2990ebec99b3b21d260aff12a16fce5f
florasss <florinasolcanu@gmail.com>
2023-04-05 20:17:42 +0300

    first commit

---
+++ oo/boxing/boxingMain.java
@@ -0,0 +1,12 @@
+package oo.boxing;
+
+public class boxingMain {
+    public static void main(String[] args){
+        Fighter fighter1 = new Fighter("Leonidas",90,25);
+        Fighter fighter2 = new Fighter("Jack",110,20);
+
+        BoxingMatch boxingMatch = new BoxingMatch(fighter1, fighter2);
+        String winner = boxingMatch.fight();
+        System.out.println("Winner is: " + winner);
+    }
+}
