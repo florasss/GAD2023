de95fbaab6d31bd474a4cdc727ff94ade0bcbe98
florasss <florinasolcanu@gmail.com>
2023-04-05 19:06:17 +0300

    FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                if (i % 3 == 0) {
+                    System.out.println("FIZZ");
+                }
+                if (i % 5 == 0) {
+                    System.out.println("BUZZ");
+                }
+            } else {
+                System.out.println(i);
+            }
+            System.out.print("");
+        }
+    }
+}
